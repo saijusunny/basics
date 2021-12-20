d229c0f5a23ea197c2a6940f4197702f6b1cd776
Saiju Sunny <saijusunny1301@gmail.com>
2021-12-20 21:01:05 +0530

    loops

--- x=4;.py
+++ x=4;.py
@@ -1,9 +1,14 @@
 a=int(input("Enter your starting Value="))
 b=int(input("Enter your final Value="))
-for x in range(a, b, 3):
-    print(x)
-else:
-    print("finally Finished")
+if 0<a and 0<b:
 
-if x<60:
-    print(len(x))
+    for x in range(a, b, 3):
+        print(x)
+    else:
+        print("finally Finished")
+elif 0>a and 0>b:
+    print ("the starting and the ending value is negative")
+elif 0>a and 0<b:
+    print ("the startying value is negative")
+elif 0<a and 0>b:   
+     print ("the final value is negative")
